49c08ae1167c3b6b940c795dfc0b877770d69ed9
MSoup <19704779+MSoup@users.noreply.github.com>
2020-11-30 18:10:54 +0900

    rps is now dynamic

--- rock-paper-scissors/index.js
+++ rock-paper-scissors/index.js
@@ -1,24 +1,35 @@
+// SCORES
+let playerScore = 0;
+let computerScore = 0;
+
+// helpers
 const CHOICES = ["rock", "paper", "scissors"];
 
+// helpers (makes comparison logic cleaner)
 const WIN_PAIRINGS = {
     "rock": "scissors",
     "scissors": "paper",
     "paper": "rock"
 }
 
+
 const computerPlay = function() {
     let choice = Math.floor(Math.random() * 3);
     return CHOICES[choice]    
 }
 
+
+
 const playRound = function(playerSelection, computerSelection) {
     let choice = playerSelection.toLowerCase();
     // Player wins
     if (WIN_PAIRINGS[choice] == computerSelection) {
+        playerScore += 1;
         return `You Win! ${capitalize(choice)} beats ${capitalize(computerSelection)}`
     }
     // Player loses
     else if (WIN_PAIRINGS[computerSelection] == choice) {
+        computerScore += 1;
         return `You Lose! ${capitalize(computerSelection)} beats ${capitalize(choice)}`
 
     }
@@ -35,11 +46,45 @@ const capitalize = function(word) {
     return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
 }
 
-const game = function() {
-    for (let i = 0; i < 5; i++) {
-        let userChoice = window.prompt("Select your weapon (rock/paper/scissors): ");
-        console.log(playRound(userChoice, computerPlay()));
-    }
+const container = document.querySelector('#container');
+
+// Make 3 buttons - Rock Paper Scissors
+for (let i = 0; i < 3; i++) {
+    var option = document.createElement('button');
+    option.innerHTML = capitalize(CHOICES[i]);
+    option.classList.add('rps-options');
+    option.value = CHOICES[i];
+    container.appendChild(option);
 }
 
-game();
+// Select all buttons and add event listeners to them
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        return logEvent(playRound(button.value, computerPlay()));
+    });
+});
+
+const scoreboard = document.querySelector('#scoreboard');
+const totals = document.querySelector('#totals');
+
+// function that logs outcome of button being pressed
+const logEvent = function(stringLog) {
+    var score = document.createElement('p');
+    // p generated with outcome of playRound (stringLog)
+    score.textContent = stringLog;
+    scoreboard.appendChild(score);
+    // display totals in heading 
+    totals.textContent = "Score: You: " + playerScore + " Computer: " + computerScore;
+    if (playerScore == 5) {
+        var conclusion = document.createElement('h2');
+        conclusion.textContent = "You win. Game over."
+        scoreboard.appendChild(conclusion);
+    }
+    else if (computerScore == 5) {
+        var conclusion = document.createElement('h2');
+        conclusion.textContent = "You lose. Game over."
+        scoreboard.appendChild(conclusion);        
+    }
+}
