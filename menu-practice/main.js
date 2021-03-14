096ae875f47cd01b16af888596bbe8dfecc6843d
MSoup <19704779+MSoup@users.noreply.github.com>
2021-03-14 16:32:11 +0900

    banner is now responsive

--- menu-practice/main.js
+++ menu-practice/main.js
@@ -1,9 +1,12 @@
 let navButton = document.querySelector(".dropdown");
 let dropdown = document.querySelector(".navContainer");
-
+let selects = dropdown.querySelectorAll("a");
 navButton.addEventListener("click", handleDisplayDropdown);
-dropdown.classList.toggle("hidden");
 
 function handleDisplayDropdown() {
   dropdown.classList.toggle("hidden");
+  // slide in each dropdown DOM element
+  selects.forEach((el) => {
+    el.classList.toggle("fade-in");
+  });
 }
